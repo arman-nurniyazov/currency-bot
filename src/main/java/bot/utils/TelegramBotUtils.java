0c1c21a29483c17b78a1c302b35aa481a2fb3fd7
Arman Nurniyazov <65914428+arman-nurniyazov@users.noreply.github.com>
2024-01-16 23:55:47 +0500

    hidden bot and phone credentials

--- src/main/java/bot/utils/TelegramBotUtils.java
+++ src/main/java/bot/utils/TelegramBotUtils.java
@@ -1,12 +1,12 @@
 package bot.utils;
 
 public interface TelegramBotUtils {
-    String BOT_TOKEN = "5067428034:AAF18WfH4nZcjltg_jimgjUR8875wiKgigM";
-    String BOT_USERNAME = "@b2_test_bot";
+    String BOT_TOKEN = "bot_token";
+    String BOT_USERNAME = "bot_username";
     String URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
-    String TWILIO_PHONENUMBER = "+14012273670";
-    String ACCOUNT_SID = "ACaf444d83089689d3d5d2ebc01dc3a2c0";
-    String AUTH_TOKEN = "66c0a7959a22dc9638922ca4bb3c638c";
+    String TWILIO_PHONENUMBER = "twilio_phone_number";
+    String ACCOUNT_SID = "account_sid";
+    String AUTH_TOKEN = "auth_token";
     String URLUSER = "d:\\Unicorn(B-2)\\Projects\\currency_bot\\src\\main\\java\\database\\User.json";
     String URLCONVERSIONHISTORY = "d:\\Unicorn(B-2)\\Projects\\currency_bot\\src\\main\\java\\database\\ConversionHistory.json";
     String PATHUSERSINFO = "C:\\Users\\99890\\Desktop\\b2\\usersInfo.xls";
